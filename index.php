dc6c2b34c477515eddbb0d6df83f555ba55486f5
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-12-25 10:03:00 +0700

    update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php require "template-parts/layouts/header.php"; ?>
 
 <!--Cảm thấy yêu mến việc học,Chiến thắng được 3 tháng bài học!-->
-<div class="uk-section-small home__block03" data-src="images/3696-Converted1.png" uk-img>
+<div class="uk-section-small home__block03" data-src="images/Group71.png" uk-img>
     <div class="uk-container">
         <div class="home__block03__txt">Cảm thấy yêu mến việc học,Chiến thắng được 3 tháng bài học!</div>
     </div>
